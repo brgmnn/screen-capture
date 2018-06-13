013b5ae73617b4d0fbfea80a8a2e19abd8e0d807
Daniel Bergmann <daniel.bergmann@send.ink>
2018-06-13 12:28:10 -0700

    Change to electron-updater for updates

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import fs from "fs";
 import path from "path";
 import { app, BrowserWindow } from "electron";
-import updater from "update-electron-app";
+import { autoUpdater } from "electron-updater";
 
-updater({ updateInterval: "1 hour" });
+autoUpdater.checkForUpdatesAndNotify();
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
